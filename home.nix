5a0b9db4d462d90ab39d9c88f9d1813ef4220d21
MartinFillon <martin.fillon@hotmail.com>
2026-05-04 22:08:30 +0200

    feat: basic nix configuration

---
+++ home.nix
@@ -0,0 +1,13 @@
+{ pkgs, pkgs-unstable, ... }:
+
+{
+    users.users.fexkoser = {
+        shell = pkgs.zsh;
+    };
+
+    home-manager.users.fexkoser = {
+        imports = [
+            ./config/hyprland.nix
+        ];
+    };
+}
