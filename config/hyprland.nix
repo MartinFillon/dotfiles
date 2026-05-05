383ab5e5ecdd9c3d5ce03aaeb6b36e9693bff505
MartinFillon <martin.fillon@hotmail.com>
2026-05-05 21:36:37 +0200

    feat: port almost all of hyprland conf to nixos

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -1,23 +1,23 @@
-{
-  ...
-}:
-
+{ ... }:
+let
+  mapping = "fr";
+in
 {
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
       "$mod" = "SUPER";
-      bind = [
-        "$mod, Q, exec, kitty"
-      ];
-      input = {
-        kb_layout = "fr";
-        follow_mouse = 1;
-        touchpad = {
-          natural_scroll = false;
-        };
-        sensitivity = 0;
+
+      bind = import ./hyprland/binds.nix { mapping = mapping; };
+      input = import ./hyprland/input.nix { mapping = mapping; };
+      general = import ./hyprland/general.nix { };
+      misc = {
+        disable_hyprland_logo = true;
       };
+      decoration = import ./hyprland/decoration.nix { };
+      animations = import ./hyprland/animations.nix { };
+
+      monitor = ",1920x1080@60,0x0,1";
     };
   };
 }
