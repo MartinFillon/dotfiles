8e1680ff4c558e2047ba4ec7496e1df0bf2c199f
MartinFillon <martin.fillon@hotmail.com>
2026-05-05 11:54:31 +0200

    feat: working home manager config

--- flake.nix
+++ flake.nix
@@ -1,8 +1,7 @@
 {
   description = "Configuration";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -11,39 +10,23 @@
 
   outputs =
     {
+      self,
       nixpkgs,
-      nixpkgs-unstable,
       home-manager,
       ...
-    }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-      pkgs-unstable = import nixpkgs-unstable {
-        inherit system;
-        config = {
-          allowUnfree = true;
-        };
-      };
-    in
+    }@inputs:
     {
-      nixosConfigurations = {
-        "desktop" = nixpkgs.lib.nixosSystem {
-          system = system;
-          modules = [
-            ./configuration.nix
-          ];
-        };
-      };
-      homeConfigurations = {
-        "desktop" = home-manager.lib.homeManagerConfiguration {
-          pkgs = pkgs;
-          modules = [
-            ./home.nix
-          ];
-        };
+      nixosConfigurations."laptop" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users."fexkoser" = import ./home.nix;
+          }
+        ];
       };
     };
 }
