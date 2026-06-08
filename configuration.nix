2aefdfe8513f80f0ba95e07cc27ec8ba724cac4d
MartinFILLON <martin.fillon@hotmail.com>
2026-06-08 13:09:35 +0200

    feat: setup new compute

--- configuration.nix
+++ configuration.nix
@@ -1,59 +1,81 @@
-{ pkgs, ... }:
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
 
 {
-  imports = [
-    # This assumes you have a hardware-configuration.nix.
-    # If testing on Arch, you can comment this out for 'build' but
-    # 'build-vm' might need it.
-    ./hardware-configuration.nix
-  ];
-
-  # Enable Flakes and the 'nix' command
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
-  virtualisation.vmVariant = {
-    virtualisation.memorySize = 4096;
-    virtualisation.cores = 2;
-  };
-
-  # Basic System Setup
-  networking.hostName = "laptop";
-  time.timeZone = "Europe/Paris";
-  services.xserver = {
-    xkb.layout = "fr";
-  };
-  console.useXkbConfig = true;
-
-  # Define your user account
-  users.users."fexkoser" = {
-    isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "networkmanager"
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
     ];
-    shell = pkgs.zsh;
-    hashedPassword = "$6$kT9luU0vpzTqg/Lj$cJhmeo1dqYxSeKbHWnIHPYqwHhBN7GtGZkg3ml8EaPw5pQD4ua1ejRal32oA8wJO7/nKp1FT/LTiPKfHiWTBQ1";
+
+  nix.settings.eperimental-features = ["nix-command" "flakes"];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
   };
 
-  # services.getty = {
-  #   autologinUser = "fexkoser";
-  # };
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "fr";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "fr";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.fexkoser = {
+    isNormalUser = true;
+    description = "fexkoser";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   services.displayManager.gdm = {
     enable = true;
   };
-  services.displayManager.defaultSession = "hyprland";
 
-  # System-wide packages
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    git
-    curl
+  	git
+  	curl
   ];
 
-  environment.pathsToLink = [
+  environments.pathsToLink = [
     "/share/applications"
     "/share/xdg-desktop-portal-hyprland"
   ];
@@ -67,8 +89,31 @@
 
   programs.zsh.enable = true;
 
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken.
-  system.stateVersion = "25.11";
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+
 }
