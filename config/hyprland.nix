d5d8a84d46b635c77417f7867f3abd965eaeac6c
MartinFILLON <martin.fillon@hotmail.com>
2026-06-08 15:20:39 +0200

    refactor(hyprland): use lua config

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -5,34 +5,38 @@ in
 {
   wayland.windowManager.hyprland = {
     enable = true;
-    configType = "hyprlang";
-    settings = {
-      exec-once = [
-        "waybar"
-      ];
-
-      "$mod" = "SUPER";
-      cursor = {
-        enable_hyprcursor = false;
-      };
-      bind = import ./hyprland/binds.nix { mapping = mapping; };
-      input = import ./hyprland/input.nix { mapping = mapping; };
-      general = import ./hyprland/general.nix { };
-      misc = {
-        disable_hyprland_logo = true;
-      };
-      decoration = import ./hyprland/decoration.nix { };
-      animations = import ./hyprland/animations.nix { };
-      monitor = "eDP-1,1920x1080@60,0x0,1";
+    configType = "lua";
+    extraLuaFiles = {
+      "main.lua" = ./hyprland/config.lua;
+      autoLoad = true;
     };
-    extraConfig = ''
-      dwindle {
-        pseudotile = yes
-        preserve_split = yes
-      }
-      ecosystem {
-        no_update_news = true;
-      }
-    '';
+    # settings = {
+    #   exec-once = [
+    #     "waybar"
+    #   ];
+
+    #   "$mod" = "SUPER";
+    #   cursor = {
+    #     enable_hyprcursor = false;
+    #   };
+    #   bind = import ./hyprland/binds.nix { mapping = mapping; };
+    #   input = import ./hyprland/input.nix { mapping = mapping; };
+    #   general = import ./hyprland/general.nix { };
+    #   misc = {
+    #     disable_hyprland_logo = true;
+    #   };
+    #   decoration = import ./hyprland/decoration.nix { };
+    #   animations = import ./hyprland/animations.nix { };
+    #   monitor = "eDP-1,1920x1080@60,0x0,1";
+    # };
+    # extraConfig = ''
+    #   dwindle {
+    #     pseudotile = yes
+    #     preserve_split = yes
+    #   }
+    #   ecosystem {
+    #     no_update_news = true;
+    #   }
+    # '';
   };
 }
