36f4e18672deb4ca05a1ce2772a0026d727d608e
MartinFILLON <martin.fillon@hotmail.com>
2026-06-10 10:06:19 +0200

    feat(dunst): config dunst to be a bit pretty

--- configuration.nix
+++ configuration.nix
@@ -114,7 +114,20 @@
     enable = true;
   };
 
-  services.dunst.enable = true;
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        width = 300;
+        height = 300;
+        offset = "30x50";
+        origin = "top-right";
+        transparency = 10;
+        frame_color = "#eceff1";
+        font = "Droid Sans 9";
+      };
+    };
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
