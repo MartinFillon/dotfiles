e6e5ca5c9a4a9cffeb3dc59eaaa6b7d74e12b215
MartinFILLON <martin.fillon@hotmail.com>
2026-08-10 12:19:01 +0200

    refactor(ssh): use nix lib to have a prettier ssh settings

--- home/config/ssh.nix
+++ home/config/ssh.nix
@@ -1,23 +1,36 @@
-{ ... }: {
+{ lib, ... }: {
   programs.ssh = {
     enable = true;
-    extraConfig = "
-    Host master.nanoforge.eu
-      HostName 92.222.226.198
-      PreferredAuthentications publickey
-      IdentityFile ~/.ssh/nanoforge-vms
-    Host worker1.nanoforge.eu
-      HostName 141.94.169.79
-      PreferredAuthentications publickey
-      IdentityFile ~/.ssh/nanoforge-vms
-    Host worker2.nanoforge.eu
-      HostName 54.38.14.251
-      PreferredAuthentications publickey
-      IdentityFile ~/.ssh/nanoforge-vms
-    Host worker3.nanoforge.eu
-      HostName 79.137.61.212
-      PreferredAuthentications publickey
-      IdentityFile ~/.ssh/nanoforge-vms
-    ";
+
+    settings =
+      lib.recursiveUpdate
+        {
+          "*" = {
+            ForwardAgent = false;
+            AddKeysToAgent = "no";
+            Compression = false;
+            ServerAliveInterval = 0;
+            ServerAliveCountMax = 3;
+            HashKnownHosts = false;
+            UserKnownHostsFile = "~/.ssh/known_hosts";
+            ControlMaster = "no";
+            ControlPath = "~/.ssh/master-%r@%n:%p";
+            ControlPersist = "no";
+          };
+        }
+        (
+          lib.mapAttrs
+            (name: value: {
+              HostName = "${value}";
+              PreferredAuthentications = "publickey";
+              IdentityFile = "~/.ssh/nanoforge-vms";
+            })
+            {
+              "master.nanoforge.eu" = "92.222.226.198";
+              "worker1.nanoforge.eu" = "141.94.169.79";
+              "worker2.nanoforge.eu" = "54.38.14.251";
+              "worker3.nanoforge.eu" = "79.137.61.212";
+            }
+        );
   };
 }
